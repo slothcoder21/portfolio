48b2174be23dbfe7204424650559b65feae5071f
Adrian Lam <adrianlam2003@gmail.com>
2024-07-29 18:30:30 -0700

    minor margin fixes

--- adrians-portfolio/app/page.js
+++ adrians-portfolio/app/page.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import Timeline from '@mui/lab/Timeline';
-import TimelineItem from '@mui/lab/TimelineItem';
-import TimelineSeparator from '@mui/lab/TimelineSeparator';
-import TimelineConnector from '@mui/lab/TimelineConnector';
-import TimelineContent from '@mui/lab/TimelineContent';
-import TimelineDot from '@mui/lab/TimelineDot';
+
 
 
 export default function Home() {
@@ -25,31 +20,15 @@ export default function Home() {
                   className="md:absolute md:mt-16"
                 />
               </Link>
-              
             </div>
             <div id="Navbar" className="mt-6 md:mt-16 text-[#858585] space-x-4 md:space-x-10 md:mr-24">
-              <Link href="#about-me" className="font-DMSans text-base md:text-lg relative
-              hover:text-black cursor-pointer transition-all ease-in-out 
-              before:transition-[width] before:ease-in-out before:duration-700 
-              before:absolute before:bg-black before:h-[1px] before:w-0 hover:before:w-full 
-              before:bottom-0 before:left-0 after:transition-[width] after:ease-in-out after:duration-700 
-              after:absolute after:bg-black after:h-[1px] after:w-0 hover:after:w-full after:bottom-0 after:right-0">
+              <Link href="#about-me" className="font-DMSans text-base md:text-lg relative hover:text-black cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-black before:h-[1px] before:w-0 hover:before:w-full before:bottom-0 before:left-0 after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-black after:h-[1px] after:w-0 hover:after:w-full after:bottom-0 after:right-0">
                 About Me.
               </Link>
-              <Link href="#projects" className="font-DMSans text-base md:text-lg relative
-              hover:text-black cursor-pointer transition-all ease-in-out 
-              before:transition-[width] before:ease-in-out before:duration-700 
-              before:absolute before:bg-black before:h-[1px] before:w-0 hover:before:w-full 
-              before:bottom-0 before:left-0 after:transition-[width] after:ease-in-out after:duration-700 
-              after:absolute after:bg-black after:h-[1px] after:w-0 hover:after:w-full after:bottom-0 after:right-0">
+              <Link href="#projects" className="font-DMSans text-base md:text-lg relative hover:text-black cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-black before:h-[1px] before:w-0 hover:before:w-full before:bottom-0 before:left-0 after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-black after:h-[1px] after:w-0 hover:after:w-full after:bottom-0 after:right-0">
                 Projects.
               </Link>
-              <Link href="https://github.com/slothcoder21/portfolio" className="font-DMSans text-base md:text-lg relative
-              hover:text-black cursor-pointer transition-all ease-in-out 
-              before:transition-[width] before:ease-in-out before:duration-700 
-              before:absolute before:bg-black before:h-[1px] before:w-0 hover:before:w-full 
-              before:bottom-0 before:left-0 after:transition-[width] after:ease-in-out after:duration-700 
-              after:absolute after:bg-black after:h-[1px] after:w-0 hover:after:w-full after:bottom-0 after:right-0">
+              <Link href="https://github.com/slothcoder21/portfolio" className="font-DMSans text-base md:text-lg relative hover:text-black cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-black before:h-[1px] before:w-0 hover:before:w-full before:bottom-0 before:left-0 after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-black after:h-[1px] after:w-0 hover:after:w-full after:bottom-0 after:right-0">
                 Source Code.
               </Link>
             </div>
@@ -65,20 +44,22 @@ export default function Home() {
         </div>
       </main>
 
-      <section id="about-me" className='w-full min-h-screen'>
-        <h1 className="text-4xl font-medium mt-16 md:mt-48 mx-6 md:mx-24 overflow-x-hidden overflow-y-hidden">
+      <section id="about-me" className="w-full min-h-screen">
+        <h1 className="text-4xl font-medium mt-16 md:mt-48 mx-6 md:mx-24">
           About Me.
         </h1>
-        <div id="Timeline" className='justify-center'>
-          
+        <div id="Timeline" className="justify-center">
+          {/* Timeline content goes here */}
         </div>
       </section>
 
-      <section id="projects" className='w-full min-h-screen'>
-        <h1 className="text-4xl font-medium mt-16 md:mt-48 mx-6 md:mx-24 overflow-x-hidden overflow-y-hidden">
+      <section id="projects" className="w-full min-h-screen">
+        <h1 className="text-4xl font-medium mt-16 md:mt-48 mx-6 md:mx-24">
           Projects.
         </h1>
+        {/* Projects content goes here */}
       </section>
+
     </>
     
   );
